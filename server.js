f8f193990f31adad5aee9b4653c653b7651a79d1
Badri Narayanan <programmingwithbadri@outlook.com>
2020-05-02 17:17:06 +0530

    Handle get request for api tours

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const fs = require('fs');
+const path =require('path');
 
 const app = express();
 
-app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
+const tours = JSON.parse(
+  fs.readFileSync(path.join(__dirname, '/dev-data/data/tours-simple.json'))
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
   });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
