a84821eda986fc520754b5751f99bf28cd2faf20
Badri Narayanan <programmingwithbadri@outlook.com>
2020-05-04 16:15:55 +0530

    serving the static files

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 
+app.use(express.static(`${__dirname}/public`));
+
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 
