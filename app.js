e3ed1bd03bef7fdf6a23afa8483e4955dd9e11f6
Badri Narayanan <programmingwithbadri@outlook.com>
2020-05-04 15:56:19 +0530

    Refactor server.js to better file structure

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const userRouter = require('./routes/userRoutes');
+const tourRouter = require('./routes/tourRoutes');
+const app = express();
+
+// Middleware
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/tours', tourRouter);
+
+module.exports = app;
