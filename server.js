c045651894be61d0fe47744feb95cf64a34bc8b2
Badri Narayanan <programmingwithbadri@outlook.com>
2020-05-11 22:54:26 +0530

    Handle unhandle promise rejections in the server

--- server.js
+++ server.js
@@ -22,6 +22,14 @@ mongoose
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
