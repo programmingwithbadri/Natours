4bfc07642d64c91ef983451051b0c8a447f901c0
Badri Narayanan <programmingwithbadri@outlook.com>
2020-05-11 19:27:04 +0530

    Handle the unhandled routes

--- app.js
+++ app.js
@@ -13,7 +13,16 @@ app.use(express.json());
 
 app.use(express.static(`${__dirname}/public`));
 
+// Routes
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
+
+// When none of the routes match,
+app.all('*', (req, res, next) => {
+  res.status(404).send({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`
+  });
+})
 
 module.exports = app;
