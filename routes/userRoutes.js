f8ce461c49f935666174efe094d43865b948ff85
Badri <programmingwithbadri@outlook.com>
2020-10-22 12:35:26 +0530

    Update the get all users route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 
-router.route('/').get(userController.getUsers).post(userController.createUser);
+router.route('/').get(userController.getAllUsers).post(userController.createUser);
 router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser);
 
 module.exports = router;
