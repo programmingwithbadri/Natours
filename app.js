a660f38677c5e3ca427d2da986133ff8fe7b9c8a
Badri Narayanan <programmingwithbadri@outlook.com>
2020-10-24 22:43:13 +0530

    Implementing Rate Limiting to limit the no of requests to the service from particular IP

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const rateLimit = require('express-rate-limit');
+
 const userRouter = require('./routes/userRoutes');
 const tourRouter = require('./routes/tourRoutes');
 
@@ -14,6 +16,15 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+ 
+app.use('/api', limiter);
 app.use(express.json());
 
 app.use(express.static(`${__dirname}/public`));
