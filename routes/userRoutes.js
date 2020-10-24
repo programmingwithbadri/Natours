e0fcdcefa6f5d483f76f5446608d2a4b0e1c4069
Badri Narayanan <programmingwithbadri@outlook.com>
2020-10-24 22:25:38 +0530

    Update the current user password route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,7 +8,12 @@ router.post('/login', authController.login);
 
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
-
+router.patch(
+    '/updateMyPassword',
+    authController.protect,
+    authController.updatePassword
+  );
+  
 router.route('/').get(userController.getUsers).post(userController.createUser);
 router.route('/').get(userController.getAllUsers).post(userController.createUser);
 router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser);
