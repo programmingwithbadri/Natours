2fb1928ee5f3fe2d02a0610d9a27f006988f7765
Badri Narayanan <programmingwithbadri@outlook.com>
2020-10-24 22:29:16 +0530

    Update and delete the current user data route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,8 +12,11 @@ router.patch(
     '/updateMyPassword',
     authController.protect,
     authController.updatePassword
-  );
-  
+);
+
+router.patch('/updateMe', authController.protect, userController.updateMe);
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
+
 router.route('/').get(userController.getUsers).post(userController.createUser);
 router.route('/').get(userController.getAllUsers).post(userController.createUser);
 router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser);
