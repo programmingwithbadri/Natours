64845cb0bad948dc43278ba4452a01b29c6ac94a
Badri Narayanan <programmingwithbadri@outlook.com>
2020-10-24 22:50:53 +0530

    Data Santization against query injection and XSS

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+
 const userRouter = require('./routes/userRoutes');
 const tourRouter = require('./routes/tourRoutes');
 
@@ -36,6 +39,12 @@ app.use('/api', limiter);
 app.use(express.json({
   limit: '10kb'
 }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
