f44e1f012dc532dd4c1a09de7ed8c92867a56a97
Badri Narayanan <programmingwithbadri@outlook.com>
2020-10-26 20:39:34 +0530

    Update the tour document  to have location data

--- models/Tour.js
+++ models/Tour.js
@@ -1,90 +1,117 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify'); // used for SEO purposes
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-    trim: true,
-    maxlength: [40, 'A tour name must have less or equal then 40 characters'],
-    minlength: [10, 'A tour name must have more or equal then 10 characters']
-  },
-  slug: String, // SEO field
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration']
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size']
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty'],
-    enum: {
-      values: ['easy', 'medium', 'difficult'],
-      message: 'Difficulty is either: easy, medium, difficult'
-    }
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-    min: [1, 'Rating must be above 1.0'],
-    max: [5, 'Rating must be below 5.0']
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price']
-  },
-  priceDiscount: {
-    type: Number,
-    validate: {
-      validator: function (val) {
-        // this only points to current doc on NEW document creation
-        return val < this.price;
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true,
+      maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+      minlength: [10, 'A tour name must have more or equal then 10 characters'],
+    },
+    slug: String, // SEO field
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult',
       },
-      message: 'Discount price ({VALUE}) should be below regular price'
-    }
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          // this only points to current doc on NEW document creation
+          return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) should be below regular price',
+      },
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a description'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image'],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
+    startLocation: {
+      // GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point'],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point'],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
   },
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'A tour must have a description']
-  },
-  description: {
-    type: String,
-    trim: true
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a cover image']
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false
-  },
-  startDates: [Date],
-  secretTour: {
-    type: Boolean,
-    default: false
+  {
+    toJSON: {
+      // mentioning when the data is converted toJson
+      // we need to include the virtual props as well(eg-durationWeeks)
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
   }
-}, {
-  toJSON: {
-    // mentioning when the data is converted toJson
-    // we need to include the virtual props as well(eg-durationWeeks)
-    virtuals: true
-  },
-  toObject: {
-    virtuals: true
-  }
-});
+);
 
 // Virtual properties are not part of DB
 // So we cannot query it anywhere
@@ -97,7 +124,7 @@ tourSchema.virtual('durationWeeks').get(function () {
 // DOCUMENT MIDDLEWARE: runs before .save() and .create()
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, {
-    lower: true
+    lower: true,
   }); // stores the human readable format of this.name value
   next();
 });
@@ -109,8 +136,8 @@ tourSchema.pre(/^find/, function (next) {
   // find the tours that doesnt have secretTour as true
   this.find({
     secretTour: {
-      $ne: true
-    }
+      $ne: true,
+    },
   });
   next();
 });
@@ -121,9 +148,9 @@ tourSchema.pre('aggregate', function (next) {
   this.pipeline().unshift({
     $match: {
       secretTour: {
-        $ne: true
-      }
-    }
+        $ne: true,
+      },
+    },
   });
 
   next();
