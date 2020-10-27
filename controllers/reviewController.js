8c4326cf6749842b2e0ce8fc38a7fd427855be54
Badri Narayanan <programmingwithbadri@outlook.com>
2020-10-27 10:35:23 +0530

    Implement simple nested routes for reviews

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,7 +2,12 @@ const Review = require('./../models/Review');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
-  const doc = await Review.find();
+  let filter = {};
+
+  // If tourId specified, then get reviews only for that Id
+  if (req.params.tourId) filter = { tour: req.params.tourId };
+
+  const doc = await Review.find(filter);
 
   // SEND RESPONSE
   res.status(200).json({
@@ -15,6 +20,13 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
 });
 
 exports.createReview = catchAsync(async (req, res, next) => {
+  // Allows nested routes
+  // If tour info not specified, get it from the params
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+
+  // If User info not specified, get it from the CurrentUser value
+  if (!req.body.user) req.body.user = req.user.id;
+
   const doc = await Review.create(req.body);
 
   res.status(201).json({
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const reviewController = require('./../controllers/reviewController');
 const authController = require('./../controllers/authController');
 
+// Merge Params will get the params of the whole URL instead of only current api path
+// tours/:tourId/reviews - wont get TourId in req.params if mergeParams is false
 const router = express.Router({ mergeParams: true });
 
 router.use(authController.protect);
